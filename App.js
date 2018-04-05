de00d4f28a83c4842a431630d3947a6f74e43010
Zay <zayseoul@gmail.com>
2018-04-05 16:39:52 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text, View } from 'react-native';
+import Component1 from './app/components/Component1/Component1';
+import Component2 from './app/components/Component2/Component2';
+import Component3 from './app/components/Component3/Component3';
+import Component4 from './app/components/Component4/Component4';
+
+export default class MyApp extends Component {
+  render() {
+    return (
+      <View>
+        <Component2 />
+        <Component3 />
+        <Component4 />
+      </View>
+    )
+  }
+}
+
+AppRegistry.registerComponent('MyApp', () => MyApp);
